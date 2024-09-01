d04a3c8f7a9ed15202a05e4224f1cba982687bca
Dan Szabo <d.szabo87@gmail.com>
2024-09-01 12:51:56 +1000

    Create data structure for items and hook up in Mine

---
+++ Source/Factory/Core/Items/ItemDataTypes.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemDataTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FItemData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Items")
+	FText ItemName;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Items")
+	FText Description;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Items")
+	UStaticMesh* Mesh = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Items")
+	UTexture* Icon = nullptr;
+	
+};
